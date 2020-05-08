0f7eea8061fc436752e9f9cf3ce3e30575ecd5fc
SaraHisham <36595602+SaraHisham@users.noreply.github.com>
2020-05-08 07:54:07 +0200

    Update main.py

--- Desktop Interface/main.py
+++ Desktop Interface/main.py
@@ -2,6 +2,7 @@ import sys
 from windows import TranslatorGUI
 from PyQt5.QtWidgets import *
 
+# App driver calling TranslatorGUI in windows.py
 
 app = QApplication(sys.argv)
 window = TranslatorGUI()
